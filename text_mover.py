b407ef06c3941368d91696ad8f1257a60597dce5
Kascald <118609415+Kascald@users.noreply.github.com>
2023-02-12 18:06:03 +0900

    code update

--- text_mover.py
+++ text_mover.py
@@ -3,12 +3,6 @@ from discord.ext import commands
 import datetime
 import os
 
-# log_ch_id = os.environ["LOG_CHANNEL"]
-# hotclip_ch_id = os.environ["HOTCLIP_CHANNEL"]
-
-# log_ch_id = 1073635083577196625
-# hotclip_ch_id = 1056575267121930290
-
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
@@ -23,6 +17,14 @@ async def on_ready():
     print("ready!")
 
     activity = discord.Game('감지')
+    all_channel = bot.get_all_channels()
+    print('all_channel   ----  ', all_channel)
+    for ch in all_channel:
+        print(f"채널명 : {ch.name} , 채널 id : {ch.id}")
+        if '핫클립' in ch.name:
+            hotclip_id = ch.id
+        if 'log' in ch.name or '로그' in ch.name or 'Log' in ch.name:
+            log_id = ch.id
     await bot.change_presence(status=discord.Status.online, activity=activity)
 
 
@@ -32,10 +34,17 @@ async def on_message_delete(message):
     guild = message.guild
     author = message.author
     channel = message.channel
+
     print('Detect message delete')
+    all_channel = bot.get_all_channels()
+    for ch in all_channel:
+        if 'log' in ch.name or '로그' in ch.name or 'Log' in ch.name:
+            log_id = ch.id
+
     if 'http' in content:
         return
-    logchannel = bot.get_channel(1073635083577196625)  # log channel id
+    # logchannel = bot.get_channel(1073463405136072764)  # log channel id
+    logchannel = bot.get_channel(log_id)
 
     n = datetime.datetime.now()
     time = f'{str(n.year)}년 {str(n.month)}월 {str(n.day)}일 {str(n.hour)}시 {str(n.minute)}분 {str(n.second)}초'
@@ -73,27 +82,38 @@ async def on_message(message):
         await message.edit(content='ㅋㅋ루삥뽕')
 
     check = []
-    if 'http' in content and message.channel != 1056575267121930290:
-        print('http링크 추정 문자열 발견')
-        for idx, val in enumerate(youtube):
-            if val in content:
-                print('유투브 링크가 맞는거 같아요!')
-                check.append(idx)
+
+    if 'http' in content:
+        all_channel = bot.get_all_channels()
+        for ch in all_channel:
+            if '핫클립' in ch.name:
+                hotclip_id = ch.id
+        # print(message.channel.id)
+        print(f"메시지 채널 타입{type(message.channel)} , 핫클립 채널id 타입 {type(hotclip_id)}")
+        if message.channel != hotclip_id:
+            print('http링크 추정 문자열 발견')
+            for idx, val in enumerate(youtube):
+                if val in content:
+                    print('유투브 링크가 맞는거 같아요!')
+                    check.append(idx)
+
         # print(len(check))
-        if len(check) != 0 and message.channel != 1056575267121930290:
-            hotclip_channel = bot.get_channel(1056575267121930290)  # hotclip channel id
+        if len(check) != 0:
+            if message.channel.id == hotclip_id:
+                return
+            hotclip_channel = bot.get_channel(hotclip_id)  # hotclip channel id
             # print(hotclip_channel)
 
             embed = discord.Embed(title=f'영상 링크가 [{message.guild.name}] 의 {channel} 에서 감지되었어요!',
                                   description='똑똑한 봇이 이동처리 할게요!', color=0x00A2F4)
             embed.add_field(name=f"영상 공유한 사람 : ", value=f"{author.nick}", inline=False)
-            embed.add_field(name='', value=f"핫클립😝 채널에 가서 확인해봐요!  👉🏻 {bot.get_channel(1056575267121930290).mention}")
+            embed.add_field(name='', value=f"핫클립😝 채널에 가서 확인해봐요!  👉🏻 {hotclip_channel.mention}")
             # hotclip channel id
 
             # print(embed)
             after = f'{author.nick} 님이 공유해주신 영상이예요. \n {content}'
             # print(after)
-            
+
             await message.delete()
             await message.channel.send(content='이 메시지는 유투브 링크이므로 봇이 이동처리 했어요!')
             await message.channel.send(embed=embed)
@@ -105,5 +125,4 @@ async def ping(message):
     await message.channel.send('pong')
 
 token = os.environ["BOT_TOKEN"]
-
 bot.run(token)
